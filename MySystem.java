86ce74741237d0a19bce4972b770e999c164ee30
azo20oz <94698181+azo20oz@users.noreply.github.com>
2021-11-19 17:57:33 +0300

    Add files via upload

---
+++ MySystem.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MySystem {
+	ArrayList<Competition> compArray = new ArrayList<Competition>();
+	
+	public void read() throws FileNotFoundException, IOException {
+
+
+
+		File file = new File("");
+		Scanner scan = new Scanner(file);
+		while (scan.hasNext()) {
+		}
+		}
+	public void write() {
+		
+	}
+	public void addCompetition() {
+		
+	}
+	public void addStudent() {
+		
+	}
+	
+	public void browse() {
+		
+	}
+	public void openEmail() {
+		
+	}
+	public void notifaction() {
+		
+	}
+	public void view(Competition comp) {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		 System.out.println("Choose an option");
+		 
+		
+	
+
+	}
+
+}
